98f0c4a76703b437e72e9135e7ab783f0b09fb09
Пучков Павел <ppuchkov@avito.ru>
2017-04-29 16:18:30 +0300

    app

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
+import json
+import logging
+
+import telebot
 from tornado.web import RequestHandler
 
 
 class MainHandler(RequestHandler):
     def get(self):
         self.write("Hello from Tornado")
+
+
+class BotHandler(RequestHandler):
+    def initialize(self, bot):
+        self.bot = bot
+
+    def get(self):
+        self.write("What are you doing here?")
+        self.finish()
+
+    def post(self):
+        if ("Content-Length" in self.request.headers
+                and "Content-Type" in self.request.headers
+                and self.request.headers['Content-Type'] == "application/json"):
+
+            # length = int(self.request.headers['Content-Length'])
+            json_data = self.request.body.decode("utf-8")
+            update = telebot.types.Update.de_json(json_data)
+            self.bot.process_new_updates([update])
+            self.write("")
+            self.finish()
+        else:
+            self.write("What are you doing here?")
+            self.finish()
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 TOKEN = '332923901:AAFZ2HhajHBTsH2a5w2lvadAIQfX2fWRB1A'
 SECRET = 'b80546a06910cb4'
+URL = 'https://ppouchkov.pythonanywhere.com'
